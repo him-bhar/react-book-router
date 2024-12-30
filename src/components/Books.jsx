5a82a9135c4f6e413c4db39430d48a3bce1d59b6
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-30 01:21:28 +0000

    How to access query parameters, mapped after the ? in a url, usually referred to as a queryString. Implemented query by title. e.g. /books?search=To

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,23 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {BooksContext} from "../BooksContext.jsx";
+import { useLocation} from "react-router-dom"; //This gives us access to request url
 
 const Books = () => {
+    const query = new URLSearchParams(useLocation().search); //This is how we get access to query url
+    const search = query.get("search") || ''; //Param name in the query url , if not available then return blank see || operator
+
     const booksCtxObj = useContext(BooksContext);
+
+    const filteredBooks = booksCtxObj.books.filter(
+        b => b.title.toLowerCase().includes(search.toLowerCase())
+    )
 
     return (
         <div>
             <h1>All Books</h1>
             <ul>
                 {
-                    booksCtxObj.books.map(book => (
+                    filteredBooks.map(book => (
                         <li key={book.id}>
                             {book.title} by {book.author}
                         </li>
