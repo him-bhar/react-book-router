414f63b6cd24e22a9552fc77251b4804495ef603
him-bhar <himanshu_bhardwajin@yahoo.co.in>
2024-12-30 00:03:34 +0000

    Setting up additional routes using Navigate element, this is for redirections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {BooksProvider} from "./BooksContext.jsx";
 import Books from "./components/Books.jsx";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 
 function App() {
 
@@ -10,10 +10,12 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Books />} />
+                    <Route path="/books" element={<Navigate to="/" /> } />
                 </Routes>
             </Router>
         </BooksProvider>
     )
 }
-
+//Navigate is used to setup redirections,
+// so basically what will happen is when we try to access url /books , we will be redirected to url /
 export default App
